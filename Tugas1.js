4fa3eee6e6caa0c7ce510d4e81508a7011e96214
Rifa Fradita Safara <146096108+Rifafradita12@users.noreply.github.com>
2025-09-17 21:10:22 +0700

    Add files via upload

---
+++ Tugas1.js
@@ -0,0 +1,16 @@
+// Luas persegi panjang
+let pjg = 12;
+let lbr = 7;
+let luasPP = pjg * lbr;
+console.log("Luas Persegi Panjang = " + luasPP);
+
+// Luas bujur sangkar
+let s = 5;
+let luasBS = s * s;
+console.log("Luas Bujur Sangkar = " + luasBS);
+
+// Luas segitiga
+let a = 8;
+let t = 6;
+let luasSeg = 0.5 * a * t;
+console.log("Luas Segitiga = " + luasSeg);
